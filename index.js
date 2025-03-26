a39e883690a3d23c1936235b5dac28da9be62e8f
B-kash <bikashchapagain047@gmail.com>
2025-03-26 18:55:21 +0100

    🎉 initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+    console.log("Document is ready");
+    // preload('/.assets/lokta.png');
+    $('#book').turn({
+        width: 1000,
+        height: 700,
+        autoCenter: true,
+        gradients: true,
+        acceleration: true,
+        inclination: 100,
+    });
+    (document.querySelector('.page') || []).forEach(page => {
+        page.style.setProperty('--background-image', image);
+    });
+});
+$(document).keydown(function (e) {
+    if (e.key === "ArrowRight") {
+        $('#book').turn('next');
+    } else if (e.key === "ArrowLeft") {
+        $('#book').turn('previous');
+    }
+});
+function goToPage(pageNum) {
+    $('#book').turn('page', pageNum);
+}
+
+function animateProgressBars() {
+    document.querySelectorAll('.progress-bar div').forEach(bar => {
+        bar.style.setProperty('--width', bar.getAttribute('data-width'));
+        bar.classList.add('animate');
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    animateProgressBars();
+});
